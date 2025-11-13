b60f032fd82646d505857e74196f008f4d02acc2
ItsJonaCR <jonavasquez.cr@gmail.com>
2025-11-13 11:04:17 -0600

    Implement CRUD operations in Spring Boot application and enhance PersonRepository with additional query methods

--- src/main/java/com/jonathan/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
+++ src/main/java/com/jonathan/springboot/jpa/springboot_jpa/SpringbootJpaApplication.java
@@ -1,13 +1,14 @@
 package com.jonathan.springboot.jpa.springboot_jpa;
 
-import java.lang.classfile.ClassFile.Option;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jonathan.springboot.jpa.springboot_jpa.entities.Person;
 import com.jonathan.springboot.jpa.springboot_jpa.repositories.PersonRepository;
@@ -22,26 +23,117 @@ public class SpringbootJpaApplication implements CommandLineRunner {
 		SpringApplication.run(SpringbootJpaApplication.class, args);
 	}
 
-	// This method is executed when the app is executed
+	// This method is called when the app is executed
 	@Override
 	public void run(String... args) throws Exception {
 
-		findOne();
-
+		delete2();
 	}
 
+	@Transactional
+	public void delete() {
+
+		repository.findAll().forEach(System.out::println);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the ID of the person to be deleted");
+		Long id = sc.nextLong();
+		sc.close();
+		repository.deleteById(id);
+
+		repository.findAll().forEach(System.out::println);
+	}
+
+	@Transactional
+	public void delete2() {
+
+		repository.findAll().forEach(System.out::println);
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the ID of the person to be deleted");
+		Long id = sc.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		optionalPerson.ifPresentOrElse(repository::delete, () -> System.out.println("The user does not exist"));
+
+		sc.close();
+
+		repository.deleteById(id);
+
+		repository.findAll().forEach(System.out::println);
+	}
+
+	@Transactional
+	public void update() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the ID of the person to be modified");
+		Long id = sc.nextLong();
+
+		Optional<Person> optionalPerson = repository.findById(id);
+
+		if (optionalPerson.isPresent()) {
+			Person p = optionalPerson.orElseThrow();
+			System.out.println("Enter the programming language of " + p.getName() + " " + p.getLastname()
+					+ " to be modified. Actual: " + p.getProgrammingLanguage());
+			String programmingLanguage = sc.next();
+			p.setProgrammingLanguage(programmingLanguage);
+			Person updatedPerson = repository.save(p);
+			System.out.println(updatedPerson);
+		} else {
+			System.out.println("The user does not exist");
+		}
+
+		// optionalPerson.ifPresent(p -> {
+		// System.out.println("Enter the programming language of " + p.getName() + " " +
+		// p.getLastname() + " to be modified. Actual: " + p.getProgrammingLanguage());
+		// String programmingLanguage = sc.next();
+		// p.setProgrammingLanguage(programmingLanguage);
+		// Person personDb = repository.save(p);
+		// System.out.println(personDb);
+
+		// });
+		sc.close();
+	}
+
+	// If you have to manipulate data in DB Tables, use Transactional
+	@Transactional
+	public void create() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name: ");
+		String name = sc.next();
+		System.out.println("Enter the lastname: ");
+		String lastname = sc.next();
+		System.out.println("Enter the programming language: ");
+		String programmingLanguage = sc.next();
+		sc.close();
+
+		// Null ID because autoincrement
+		Person person = new Person(null, name, lastname, programmingLanguage);
+
+		Person newPerson = repository.save(person);
+		System.out.println(newPerson);
+
+		repository.findById(person.getId()).ifPresent(System.out::println);
+	}
+
+	// When it is only a read transaction, use readOnly = true
+	@Transactional(readOnly = true)
 	public void findOne() {
 
-		// Person person = null;
-		// Optional<Person> optionalPerson = repository.findById(1L);
-		// if(optionalPerson.isPresent()) {
-		// 	person = optionalPerson.get();
-		// }
-		// System.out.println(person);
+		Person person = null;
+		Optional<Person> optionalPerson = repository.findByNameContaining("se");
+		if (!optionalPerson.isEmpty()) {
+			person = optionalPerson.get();
+		}
+		System.out.println(person);
 
-		repository.findById(1L).ifPresent(System.out::println);
+		// repository.findOneName("Pepe").ifPresent(System.out::println);
 	}
 
+	@Transactional(readOnly = true)
 	public void list() {
 		// List<Person> persons = (List<Person>) repository.findAll();
 		// List<Person> persons = (List<Person>)
--- src/main/java/com/jonathan/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
+++ src/main/java/com/jonathan/springboot/jpa/springboot_jpa/repositories/PersonRepository.java
@@ -1,6 +1,7 @@
 package com.jonathan.springboot.jpa.springboot_jpa.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,6 +9,23 @@ import org.springframework.data.repository.CrudRepository;
 import com.jonathan.springboot.jpa.springboot_jpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
+
+    // CHECK DOCUMENTATION FOR QUERY METHODS IN SPRING DATA
+    
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    // like: used to filter and the "%" is used to search from the left to the right of the word 
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
+    Optional<Person> findByName(String name);
+
+    // same as using like but without the @Query already declared by spring data
+    Optional<Person> findByNameContaining(String name);
 
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
